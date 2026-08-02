05882fc9a75bcd7fe19787c2b265a8059a3ef31c
Your Name <you@example.com>
2026-08-02 09:48:04 +0530

    chore: remove unnecessary tsconfig.tsbuildinfo file

--- .backup-unused-files/scripts/gen-hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-const password = 'Pranee@1909';
-bcrypt.hash(password, 12).then(hash => {
-  console.log('HASH:', hash);
-  // Verify it works
-  bcrypt.compare(password, hash).then(ok => {
-    console.log('Verify:', ok);
-  });
-});
